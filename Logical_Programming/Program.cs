5ef8820050792cc8c4cfba0e7e6b16c8c9b88d2d
ammu72642668 <arundhamodepalli726426@gmail.com>
2023-02-25 07:49:56 +0530

    Prime number in logical programming

--- Logical_Programming/Program.cs
+++ Logical_Programming/Program.cs
@@ -4,8 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Logical Programming");
-            FibonacciSeries.Fibonacci_Series();
+            //Console.WriteLine("Welcome to Logical Programming");
+            //FibonacciSeries.Fibonacci_Series();
+            PerfectNumber.Perfect_Number();
         }
     }
 }
