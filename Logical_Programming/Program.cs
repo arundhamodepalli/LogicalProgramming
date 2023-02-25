3ca697fb670b28ade2e363577f3954053415be92
ammu72642668 <arundhamodepalli726426@gmail.com>
2023-02-25 07:55:27 +0530

    reverse number in logical programming

--- Logical_Programming/Program.cs
+++ Logical_Programming/Program.cs
@@ -6,7 +6,8 @@
         {
             //Console.WriteLine("Welcome to Logical Programming");
             //FibonacciSeries.Fibonacci_Series();
-            PerfectNumber.Perfect_Number();
+            // PerfectNumber.Perfect_Number();
+            PrimeNumber.Prime_Number();
         }
     }
 }
